19261d3cb7f069d1cc57b5ae08121893eaec7993
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-01-26 07:37:10 +0300

    Update SSgen.py

--- SSgen.py
+++ SSgen.py
@@ -1,9 +1,9 @@
   
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-print.white.italic ("Wyline String Alıcıya Hoşgeldiniz...")
-print.bold.blue ("SupportGroup:@wylinesupport")
-print.red.italic ("""Biraz Bekleyin...""")
+print.white.italic ("")
+print.bold.blue ("")
+print.red.italic (""" Wyline String Alıcıya Hoşgeldiniz SupportGroup:@wylinesupport""")
 
 API_KEY = '1754367'
 API_HASH = "231b8cc6cca12ee51a85cf543321f476"
